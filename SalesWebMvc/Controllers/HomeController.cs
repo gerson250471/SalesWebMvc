754528e628fa64fcc43353e6961ffe7a9aa69acd
Gerson Bernardo <gbds25@gmail.com>
2022-01-06 15:05:22 -0300

    Primeiro Controlador e páginas Razor

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "  Sistemas de Vendas ";
+            ViewData["Responsavel"] = "Gerson Bernardo da Silva";
 
             return View();
         }
