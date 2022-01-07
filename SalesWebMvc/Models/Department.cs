ca939724c960830a4f9724d512a161fc01ce3e19
Gerson Bernardo <gbds25@gmail.com>
2022-01-06 22:56:04 -0300

    Outras Entidades e Segundo Migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,8 +1,34 @@
-﻿namespace SalesWebMvc.Models
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            sellers.Add(seller);  
+        }
+
+        public double TotalSales(DateTime initial,DateTime final)
+        {
+            return sellers.Sum(Seller => Seller.TotalSales(initial,final));
+        }
+
     }
 }
