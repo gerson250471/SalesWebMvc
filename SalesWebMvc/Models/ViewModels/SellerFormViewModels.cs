5def418aa8b39e8f52173dc8d2debc383dddb811
Gerson Bernardo <gbds25@gmail.com>
2022-01-08 18:19:50 -0300

    SellerFormViewModel e Seleção de Departamentos

---
+++ SalesWebMvc/Models/ViewModels/SellerFormViewModels.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SalesWebMvc.Models.ViewModels
+{
+    public class SellerFormViewModels
+    {
+        public Seller seller { get; set; }
+        public ICollection<Department> Departments { get; set; }
+    }
+}
