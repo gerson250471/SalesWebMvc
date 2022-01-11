8a3055674a6d47ef808bff4d10621fe2954c4a71
Gerson Bernardo <gbds25@gmail.com>
2022-01-11 10:33:50 -0300

    Validação dos dados

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -8,14 +8,20 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Campo {0} Obrigatório")]
+        [StringLength(70, MinimumLength =3, ErrorMessage ="Tamanho do {0} deve ficar entre {2} e {1} caracteres")]
         public string Name { get; set; }
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Campo {0} Obrigatório")]
+        [EmailAddress(ErrorMessage ="Entre com um {0} Valido")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Campo {0} Obrigatório")]
         [Display(Name = "Data Nascimento")]
         [DataType(DataType.Date)]    
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "Campo {0} Obrigatório")]
         [Display(Name="Salário Base")]
         [DisplayFormat(DataFormatString ="{0:F2}")]
         public double BaseSalary { get; set; }
